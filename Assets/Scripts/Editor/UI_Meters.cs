3c604874668cdc157cf852bf35d217ee3abdbde7
= <jake@mcleman.com>
2021-09-18 22:48:37 -0700

    Editor options to insert meters

--- Assets/Scripts/Editor/UI_Meters.cs
+++ Assets/Scripts/Editor/UI_Meters.cs
@@ -9,13 +9,40 @@ public class UI_Meters : MonoBehaviour
     {
         GameObject donut = new GameObject("Donut Meter");
         donut.transform.parent = Selection.activeTransform;
+
         Image image = donut.AddComponent<Image>();
         image.sprite = (Sprite)AssetDatabase.LoadAssetAtPath("Assets/Sprites/UI/UI_DonutBar.png", typeof(Sprite));
         image.type = Image.Type.Filled;
         image.fillMethod = Image.FillMethod.Radial360;
         image.fillOrigin = 2;
 
+        FillMeter fill = donut.AddComponent<FillMeter>();
+        fill.bar = image;
+        fill.minVal = 0;
+        fill.maxVal = 1;
+
         Undo.RegisterCreatedObjectUndo(donut, "Create " + donut.name);
         Selection.activeObject = donut;
+    }
+
+    [MenuItem("GameObject/UI/Meters/Bar Meter")]
+    public static void CreateBarMeter()
+    {
+        GameObject bar = new GameObject("Bar Meter");
+        bar.transform.parent = Selection.activeTransform;
+
+        Image image = bar.AddComponent<Image>();
+        image.sprite = (Sprite)AssetDatabase.LoadAssetAtPath("Assets/Sprites/UI/UI_BasicBox.png", typeof(Sprite));
+        image.type = Image.Type.Filled;
+        image.fillMethod = Image.FillMethod.Horizontal;
+        image.fillOrigin = 1;
+
+        FillMeter fill = bar.AddComponent<FillMeter>();
+        fill.bar = image;
+        fill.minVal = 0;
+        fill.maxVal = 1;
+
+        Undo.RegisterCreatedObjectUndo(bar, "Create " + bar.name);
+        Selection.activeObject = bar;
     }
 }
